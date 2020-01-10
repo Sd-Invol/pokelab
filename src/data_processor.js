50de392a499ab961e56b00e96f9172e48f234984
Sd-Invol <sd.invol@gmail.com>
2020-01-10 17:08:59 +0800

    Minor fix

--- src/data_processor.js
+++ src/data_processor.js
@@ -1,8 +1,6 @@
-import React from 'react';
-
 import pokemons from './data/pokemons';
-import abilities from './data/abilities';
 import moves from './data/moves';
+import types from './data/types';
 
 let pokemonSelectList = [];
 const duplicatedForms = [25, 773, 778, 845, 855, 869, 875, 890];
@@ -26,5 +24,39 @@ for (let idx in pokemons) {
     pokemonSelectList.push(select);
 }
 
-export { pokemonSelectList };
+let types_cn_to_en = {};
+for (let type in types) {
+    types_cn_to_en[types[type].cn] = type;
+}
 
+for (let idx in pokemons) {
+    let pokemon = pokemons[idx];
+
+    let moveByType = {};
+    for (let moveId of pokemon.moves) {
+        const move = moves[moveId];
+        if (move.class === '变化') {
+            continue;
+        }
+        if (!(move.type in moveByType)) {
+            moveByType[move.type] = {
+                physical: [],
+                special: [],
+            }
+        }
+        if (move.class === '物理') {
+            moveByType[move.type].physical.push(move);
+        }
+        if (move.class === '特殊') {
+            moveByType[move.type].special.push(move);
+        }
+    }
+    for (let type in moveByType) {
+        moveByType[type].physical = moveByType[type].physical.sort(
+            (a, b) => Number(b.power) - Number(a.power));
+        moveByType[type].special = moveByType[type].special.sort(
+            (a, b) => Number(b.power) - Number(a.power));
+    }
+}
+
+export { pokemonSelectList };
